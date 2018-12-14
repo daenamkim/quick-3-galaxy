e0263b19c37f51bdf2702b177910c6e0ae8d308d
Daenam Kim <daenam.kim@gmail.com>
2018-12-14 22:50:11 +0900

    Simplify audios.

--- src/util/sound.js
+++ src/util/sound.js
@@ -1,40 +1,16 @@
 const BACKGROUND = "background";
 const LASER = "laser";
 const TADA = "tada";
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-const audioBuffer = {};
 
 function play(which) {
-  const source = audioCtx.createBufferSource();
-  const request = new XMLHttpRequest();
-  request.open("GET", `/sound/${which}.mp3`);
-  request.responseType = "arraybuffer";
-  if (audioBuffer[which] === undefined) {
-    request.onload = () => {
-      audioCtx.decodeAudioData(
-        request.response,
-        (buffer) => {
-          audioBuffer[which] = buffer;
-          source.buffer = buffer;
-          source.connect(audioCtx.destination);
-          // source.loop = true;
-        },
-        (err) => {
-          console.log(`Audio decoding error: ${err.err}`);
-        }
-      );
-    };
-    request.send();
-  } else {
-    source.buffer = audioBuffer[which];
-  }
-  source.connect(audioCtx.destination);
-  source.start(0);
+  const source = new Audio(`/sound/${which}.mp3`);
+  source.play();
   return source;
 }
 
 function stop(source) {
-  source.stop(0);
+  source.pause();
+  source.currentTime = 0;
 }
 
 module.exports = {
